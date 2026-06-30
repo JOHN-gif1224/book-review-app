bf4c9e610c883ad96035cbade9f97c30a7bc7540
KATIN John <johnpkatin@gmail.com>
2026-06-30 06:55:20 +0000

    Création du serveur Express JS

---
+++ index.js
@@ -0,0 +1,22 @@
+// Importation de Express
+const express = require("express");
+
+// "Instance" de l'application Express (Objet central qui va gérer les routes et middlewares)
+const app = express();
+
+// Définition du port sur lequel le serveur va écouter
+const PORT = 5000;
+
+// MIDDLEWARE: permet à Express de comprendre le JSON envoyé dans le corps des requêtes
+// Sans ça, req.body serait "undefined"
+app.use(express.json());
+
+// ROUTE DE TEST: c'est quand on fait un GET sur "/"
+app.get("/", (req, res) => {
+    res.send("Bienvenu sur l'API Book Review !");
+})
+
+// Démarrage du serveur, qui écoute sur le PORT
+app.listen(PORT, () => {
+    console.log(`Serveur démarrer sur http://localhost:${PORT}`);
+})
